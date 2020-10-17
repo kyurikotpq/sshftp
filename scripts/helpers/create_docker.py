1175e0106fea18a550f564a4004060d495775ced
Kyuriko Tea Pei Qi <47902092+kyurikotpq@users.noreply.github.com>
2020-10-17 21:44:06 +0800

    fix: Add auto-restart of Docker container

--- scripts/helpers/create_docker.py
+++ scripts/helpers/create_docker.py
@@ -5,6 +5,7 @@ def create_docker(CHROOT_DIR, PORT, username, password):
     COMMAND_LINES = [
         f"sudo docker run -d -v {CHROOT_DIR}/html:/var/www/localhost/htdocs/",
         f"-e USERNAME={username} -e MYSQL_ROOT_PASSWORD={password}",
+        f"--restart always",
         f'-p {PORT}:80 --name {username} kyurikotpq/lampm2'
     ]
 
